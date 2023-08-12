e5c15e02ce3576c0901a1c1af9b9484517f75715
ganichev <Ganichev@lanit.ru>
2023-08-12 23:47:24 +0300

    6.7 Практическая работа

--- 06_objects/task_3.js
+++ 06_objects/task_3.js
@@ -3,13 +3,16 @@ let objects = [
   { name: 'Иван', surname: 'Иванов' },
   { name: 'Пётр', surname: 'Петров' }
 ]
- 
 
-function filter(arr, prop, value){
-  // Здесь решение задачи
-  // arr - массив объектов
-  // prop - свойство по которому производится фильтрация
-  // value - значение свойства по которому производится фильтрация
+
+function filter(arr, prop, value) {
+  let result = [];
+  for (const object of arr) {
+    if (object[prop] === value) {
+      result.push(object);
+    }
+  }
+  return result;
 }
 
 let result = filter(objects, 'name', 'Иван');
